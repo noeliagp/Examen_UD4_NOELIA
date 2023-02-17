ef015647aacdd0a39cdf41489d62483a2506ca1b
noeliagp <noeliadelcarmengonzalezpardo@gmail.com>
2023-02-17 12:27:32 +0100

    PROYECTO CON IMÁGENES

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,28 @@
-package org.example;public class Main {
+package org.example;
+/**
+ * @author Noelia 1ºDAW
+ *
+ */
+public class Main {
+    public static double ej9(char tipo, double sueldo, int antiguedad) {
+        double incremento;
+        switch(tipo) {
+            case 'E': incremento=sueldo*1.0;
+            case 'N': incremento=sueldo*0.5;
+            default: incremento=0.0;
+        }
+        if (antiguedad>24)
+            incremento=incremento*antiguedad/10;
+        else
+        if (antiguedad>12)
+            incremento=incremento*antiguedad/20;
+        else
+            incremento=incremento*antiguedad/30;
+        return sueldo+incremento; // línea donde establecer un punto de ruptura, breakpoint
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ej9('N',1000,14);
+
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,4 +1,21 @@
+package org.example;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MainTest {
-  
+
+    @org.junit.jupiter.api.Test
+    void ej9() {
+       // assertNotEquals(1000,Main.ej9('N',1000,1)); // este método falla porque el resultado es 1000 cuando tiene que ser distinto a 1000 en este caso 1016 euros
+      //  assertNotEquals(1001,Main.ej9('E',1001,1)); // este método falla porque el resultado 1001 cuando tiene que ser distinto 1001 en este caso 1033
+        assertEquals(1000,Main.ej9('R',1000,1));
+       assertEquals(1000,Main.ej9('N',1000,0));
+      assertEquals(999,Main.ej9('N',999,1));
+
+
+        // Llamar al método con las siguientes variables N, 1000,14
+
+    }
+
+
 }
